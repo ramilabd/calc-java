ebc8e57f16eef4b1cbbcb258158857fd393e38de
ramilabd <ramsmart@mail.ru>
2022-05-17 12:10:15 +0500

    feat: added class InputOutput

---
+++ src/com/validate/InputOutput.java
@@ -0,0 +1,24 @@
+package com.validate;
+
+import java.util.Scanner;
+
+public class InputOutput {
+
+    public String getExpression() {
+        Scanner in = new Scanner(System.in);
+        String expression;
+
+        System.out.print("Ввод: ");
+        expression = in.nextLine();
+
+        if (expression.equals("q")) {
+            in.close();
+        }
+
+        return expression;
+    }
+
+    public void displayResult(int result) {
+        System.out.println("Результат: " + result);
+    }
+}
