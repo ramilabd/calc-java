6c6672dcb9203895b21f6580abc80f866b0adf7d
ramilabd <ramsmart@mail.ru>
2022-05-17 12:06:13 +0500

    feat: added class Greeting

---
+++ src/com/Greeting.java
@@ -0,0 +1,16 @@
+package com;
+
+public class Greeting {
+
+    static String greeting = """
+        Калькулятор.
+        Введите математическое выражение вида "{число} {опрартор} {число}"
+        Число: арабское или римское, от 0 до 10
+        Оператор: +, -, *, /
+        Для выхода из программы введите: 'q'
+        """;
+
+    public static void displayGreeting() {
+        System.out.println(greeting);
+    }
+}
