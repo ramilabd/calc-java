ab6cfc0f23106aed9b2f2371751736ae6fecd927
ramilabd <ramsmart@mail.ru>
2022-05-19 00:26:06 +0500

    refactor: refactor of method "start" and constants
    move constants (templates) to a class Validate
    refacor methods "start"

--- src/com/engine/Engine.java
+++ src/com/engine/Engine.java
@@ -1,5 +1,6 @@
 package com.engine;
 
+import com.calculator.Calculator;
 import com.parsing.Expression;
 import com.parsing.Parser;
 import com.validate.InputOutput;
@@ -7,36 +8,22 @@ import com.validate.Validate;
 
 public class Engine {
 
-    final String PATTERN_ARABIC = "\\s*(10|[0-9])\\s*(\\+|\\-|\\*|\\/)\\s*(10|[0-9])\\s*";
-    final String PATTERN_ROMAN = "\\s*\\b(I|II|III|IV|V|VI|VII|VIII|IX|X)\\b\\s*(\\+|\\-|\\*|\\/)\\s*\\b(I|II|III|IV|V|VI|VII|VIII|IX|X)\\b\\s*";
     Expression cleared_expression;
     InputOutput io = new InputOutput();
-    Validate validate = new Validate(PATTERN_ARABIC, PATTERN_ROMAN);
+    Validate validate = new Validate();
     Parser parser = new Parser();
+    Calculator calculator = new Calculator();
 
-    public void start() {
-        byte result = 0;
+    public void start() throws Exception {
         String expression = "";
 
         while (!expression.equals("q")) {
             expression = io.getExpression();
 
-            if (validate.isMatchesTemplateArabic(expression)) {
-                cleared_expression = parser.parse(expression, "arabic");
-            } else if (validate.isMatchesTemplateRoman(expression)) {
-                cleared_expression = parser.parse(expression, "roman");
-            }
+            String pattern = validate.validator(expression);
 
-            byte firstOperator = cleared_expression.getFirstOperand();
-            byte secondOperator = cleared_expression.getSecondOperand();
-            char operator = cleared_expression.getOperator();
-
-            switch (operator) {
-                case '+' -> result = (byte) (Math.addExact(firstOperator, secondOperator));
-                case '-' -> result = (byte) (Math.subtractExact(firstOperator, secondOperator));
-                case '*' -> result = (byte) (Math.multiplyExact(firstOperator, secondOperator));
-                case '/' -> result = (byte) (Math.floorDiv(firstOperator, secondOperator));
-            }
+            cleared_expression = parser.parse(expression, pattern);
+            String result = calculator.calculate(cleared_expression, pattern);
 
             io.displayResult(result);
         }
--- src/com/validate/Validate.java
+++ src/com/validate/Validate.java
@@ -1,30 +1,51 @@
 package com.validate;
+
+import java.util.HashMap;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 
 public class Validate {
 
-    String patternArabic;
-    String patternRoman;
+    public String validator(String inputExpression) throws Exception {
 
-    public Validate(String patternArabic, String patternRoman){
-        this.patternArabic = patternArabic;
-        this.patternRoman = patternRoman;
+        HashMap<String, String> patterns = new HashMap<>();
+
+        patterns.put("PATTERN_ARABIC", "\\s*(10|[0-9])\\s*(\\+|\\-|\\*|\\/)\\s*(10|[0-9])\\s*");
+        patterns.put("PATTERN_ROMAN",
+            "\\s*\\b(I|II|III|IV|V|VI|VII|VIII|IX|X)\\b\\s*(\\+|\\-|\\*|\\/)\\s*\\b(I|II|III|IV|V|VI|VII|VIII|IX|X)\\b\\s*");
+        patterns.put("PATTERN_ROM_ARAB",
+            "\\s*\\b(I|II|III|IV|V|VI|VII|VIII|IX|X)\\b\\s*(\\+|\\-|\\*|\\/)\\s*(10|[0-9])\\s*");
+        patterns.put("PATTERN_ARAB_ROM",
+            "\\s*(10|[0-9])\\s*(\\+|\\-|\\*|\\/)\\s*\\b(I|II|III|IV|V|VI|VII|VIII|IX|X)\\b\\s*");
+        patterns.put("PATTERN_ONE_NUM",
+            "\\s*(10|[0-9])|\\b(I|II|III|IV|V|VI|VII|VIII|IX|X)\\b\\s*");
+        patterns.put("PATTERN_ARABIC_MORE_TWO_NUM",
+            "\\s*(10|[0-9])\\s*(\\+|\\-|\\*|\\/)\\s*(10|[0-9])\\s*(\\+|\\-|\\*|\\/)\\s*(10|[0-9])\\s*");
+        patterns.put("PATTERN_ROMAN_MORE_TWO_NUM",
+            "\\s*\\b(I|II|III|IV|V|VI|VII|VIII|IX|X)\\b\\s*(\\+|\\-|\\*|\\/)\\s*\\b(I|II|III|IV|V|VI|VII|VIII|IX|X)\\b\\s*(\\+|\\-|\\*|\\/)\\s*\\b(I|II|III|IV|V|VI|VII|VIII|IX|X)\\b\\s*");
+//        patterns.put("ANY_CHARACTER", "[A-Za-z0-9]");
+        String result = null;
+
+        for (String key : patterns.keySet()) {
+            Pattern pattern = Pattern.compile(patterns.get(key));
+            Matcher matcher = pattern.matcher(inputExpression);
+
+            if (matcher.matches()) {
+                result = switch (key) {
+                    case "PATTERN_ARABIC", "PATTERN_ROMAN" -> key;
+                    case "PATTERN_ROM_ARAB", "PATTERN_ARAB_ROM" ->
+                        throw new Exception("Используются одновременно разные системы счисления.");
+                    case "PATTERN_ONE_NUM" ->
+                        throw new Exception("Строка не является математической операцией");
+                    case "PATTERN_ARABIC_MORE_TWO_NUM", "PATTERN_ROMAN_MORE_TWO_NUM" ->
+                        throw new Exception(
+                            "Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+                    default -> throw new Exception(
+                        "Не гоните ерунду :), читайте описание работы калькулятора");
+                };
+            }
+        }
+        return result;
     }
-
-    public boolean isMatchesTemplateArabic(String input) {
-        Pattern numArabic = Pattern.compile(patternArabic);
-        Matcher matcherArabic = numArabic.matcher(input);
-
-        return matcherArabic.matches();
-    }
-
-    public boolean isMatchesTemplateRoman(String input) {
-        Pattern numRoman = Pattern.compile(patternRoman);
-        Matcher matcherRoman = numRoman.matcher(input);
-
-        return matcherRoman.matches();
-    }
-
 }
