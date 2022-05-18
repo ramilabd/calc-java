303670be5c86c837a709cefe4f41f8aba3209d17
ramilabd <ramsmart@mail.ru>
2022-05-19 00:31:05 +0500

    refactor: add Exception to method "main"

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import com.engine.Engine;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Greeting.displayGreeting();
 
         Engine engine = new Engine();
